4630c2a62ebd278d85e8f5812cd7f50a0f662920
Gowtham-Chokkalingam <gowthamtceian@gmail.com>
2022-12-29 21:57:39 +0530

    payment deleted

--- Backend/routes/route.js
+++ Backend/routes/route.js
@@ -1,5 +1,4 @@
 import express from "express";
-import { addPaymentGateway, paymentResponse } from "../controller/payment-controller.js";
 import { getProductById, getProducts } from "../controller/product-controller.js";
 import { userLogin, userSignup } from "../controller/user-controller.js";
 
@@ -11,7 +10,6 @@ router.post("/login", userLogin);
 router.get("/products", getProducts);
 router.get("/product/:id", getProductById);
 
-router.post(`/payment`, addPaymentGateway);
-router.post("/callback", paymentResponse);
+
 
 export default router;
